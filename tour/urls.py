f47652c35e40aa5e4457f92abdd34be7476ed127
dark <drssvd2@gmail.com>
2021-02-18 12:19:56 +0330

    Handle listing pages

--- tour/urls.py
+++ tour/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, tour_detail, city_detail
+from .views import home, tour_detail, city_detail, tour_listing, city_lising
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('tours/<slug:slug>',tour_detail, name='tour-detail'),
     path('city/<slug:slug>', city_detail, name='city-detail'),
+    path('CityListing', city_lising, name='city-listing'),
+    path('TourListing', tour_listing, name='tour-listing'),
 ] + static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
--- tour/views.py
+++ tour/views.py
@@ -3,22 +3,38 @@ from .models import TourModel, CityModel
 
 # Create your views here.
 def home(request):
-    context = {}
-    context['a'] = 'test'
-    context['random_tours'] = TourModel.objects.filter(is_active=True).order_by('?')
-    context['top_tours'] = TourModel.objects.filter(is_active=True).order_by('-score')
-    context['cities'] = CityModel.objects.order_by('?')
+    context = {
+    'a' : 'test',
+    'random_tours' : TourModel.objects.filter(is_active=True).order_by('?'),
+    'top_tours' : TourModel.objects.filter(is_active=True).order_by('-score'),
+    'cities' : CityModel.objects.order_by('?'),
+    }
     return render(request, 'index.html',context)
 
 
 def tour_detail(request, slug):
-    context = {}
-    context['tour'] = get_object_or_404(TourModel, slug=slug)
-    context['tours'] = TourModel.objects.filter(is_active=True).order_by('-score')
+    context = {
+    'tour' : get_object_or_404(TourModel, slug=slug),
+    'tours' : TourModel.objects.filter(is_active=True).order_by('-score'),
+    }
     return render(request, 'tour-detail.html',context)
 
 def city_detail(request, slug):
-    context = {}
-    context['city'] = get_object_or_404(CityModel, slug=slug)
-    context['tours'] = TourModel.objects.filter(is_active=True).order_by('-score')
+    context = {
+    'city' : get_object_or_404(CityModel, slug=slug),
+    'tours' : TourModel.objects.filter(is_active=True).order_by('-score'),
+    }
     return render(request, 'city-detail.html',context)
+
+def tour_listing(request):
+    context={
+        'tours': TourModel.objects.filter(is_active=True).order_by('-date_created'),
+    }
+    return render(request, 'tour-listing.html',context)
+    
+def city_lising(request):
+    context = {
+        'cities' : CityModel.objects.all()[:5],
+        'tours': TourModel.objects.filter(is_active=True).order_by('-date_created'),
+    }
+    return render(request, 'city-listing.html', context)
