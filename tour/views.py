1f6744c7918d83d3e5ebe3d91c4ffd345cc8aa85
dark <drssvd2@gmail.com>
2021-02-20 09:30:37 +0330

    Add pagination to cities and tour listing page

--- tour/views.py
+++ tour/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import TourModel, CityModel
 
 # Create your views here.
@@ -27,14 +28,23 @@ def city_detail(request, slug):
     return render(request, 'city-detail.html',context)
 
 def tour_listing(request):
+    tours_page = TourModel.objects.filter(is_active=True).order_by('-date_created')
+    paginator = Paginator(tours_page, 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context={
-        'tours': TourModel.objects.filter(is_active=True).order_by('-date_created'),
+        'tours': page_obj,
     }
     return render(request, 'tour-listing.html',context)
     
 def city_lising(request):
+    # Pagination
+    cities = CityModel.objects.all()
+    paginator = Paginator(cities, 1)
+    page_num = request.GET.get('page')
+    page_obj = paginator.get_page(page_num)
     context = {
-        'cities' : CityModel.objects.all()[:5],
+        'cities' : page_obj,
         'tours': TourModel.objects.filter(is_active=True).order_by('-date_created'),
     }
     return render(request, 'city-listing.html', context)
